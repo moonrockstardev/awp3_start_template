006be17efe5efe329c5d374c776963adcf9fe960
andriiwp3 <andriiwp3@gmail.com>
2020-09-07 10:45:14 +0300

    Remade popup & header adaptive

--- #src/js/jq-start.js
+++ #src/js/jq-start.js
@@ -15,3 +15,19 @@ $(document).ready(function() {
 	if(isMobile.any()){
 		$('body').addClass('touch');
 	}
+	function testWebP(callback) {
+		let webP = new Image();
+		webP.onload = webP.onerror = function () {
+			callback(webP.height == 2);
+		};
+		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+	}
+	
+	testWebP(function(support) {
+	
+		if (support == true) {
+			document.querySelector('body').classList.add('webp');
+		} else {
+			document.querySelector('body').classList.add('no-webp');
+		}
+	});
